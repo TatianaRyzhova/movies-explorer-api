c622886d5d90b175a4c7f8658063156db9b15594
Tatiana Ryzhova <tatiana.ryzhova@deliveryhero.com>
2021-05-05 21:54:09 +0200

    add dependencies & db models for user & movie

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'The name cannot be less than two characters'],
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Invalid mail format',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [8, 'Password must be at least 8 characters'],
+    select: false,
+  },
+}, {versionKey: false});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({email}).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Incorrect email or password'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Incorrect email or password'));
+          }
+          return user;
+        });
+    });
+};
+
+function toJSON() {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+}
+
+userSchema.methods.toJSON = toJSON;
+
+module.exports = mongoose.model('user', userSchema);
